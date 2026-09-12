0259c5d2f88f93dbca7fd208b08424880c707f3c
Jinhao Xu <x@qntx.fun>
2026-09-12 15:42:23 +0800

    test: CRC-32/ISO-HDLC catalogue vector
    Assert checksum of ASCII 123456789 equals 0xcbf43926.

--- tests/crc32.test.ts
+++ tests/crc32.test.ts
@@ -4,4 +4,5 @@ import { checksum } from "../src/crc32.ts";
 test("known CRC-32 ISO-HDLC vectors", () => {
   expect(checksum(new TextEncoder().encode("Hello, world!"))).toBe(0xebe6c6e6);
   expect(checksum(new TextEncoder().encode("Wolf"))).toBe(0x598c84dc);
+  expect(checksum(new TextEncoder().encode("123456789"))).toBe(0xcbf43926);
 });
